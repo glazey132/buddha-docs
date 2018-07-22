148ed8b60dc75058af059cd30139d71066d3955a
Alex <alglaze132@gmail.com>
2018-07-22 13:39:20 -0700

    import App.js into app.js

--- reactApp/app.js
+++ reactApp/app.js
@@ -3,28 +3,7 @@ var ReactDOM = require('react-dom');
 import { MemoryRouter } from 'react-router-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
-require('./css/main.css');
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <BrowserRouter>
-        <div>
-          <Switch>
-            <Route path="/" exact component={Login} />
-            <Route path="/login" exact component={Login} />
-            <Route path="/register" exact component={Register} />
-            <Route path="/editDocument/:docid" component={DocumentContainer} />
-            <Route path="/userDocs/:userid" component={DocumentPortal} />
-          </Switch>
-        </div>
-      </BrowserRouter>
-    );
-  }
-}
+import App from './Components/App';
 
 ReactDOM.render(
   <MuiThemeProvider>
