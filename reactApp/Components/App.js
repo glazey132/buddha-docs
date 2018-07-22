eb303b64bfc78621ec923d6408bc6221c71ca5a4
Alex <alglaze132@gmail.com>
2018-07-22 15:25:47 -0700

    App constructor

--- reactApp/Components/App.js
+++ reactApp/Components/App.js
@@ -1,2 +1,12 @@
 import React from 'React';
 import { Route } from 'react-router-dom';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div />;
+  }
+}
