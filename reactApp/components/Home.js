5faa2506d83574c96f3515b84be05cbde3dc9e3e
Alex <alglaze132@gmail.com>
2018-07-25 12:59:28 -0700

    start of reorganizing homepage

--- reactApp/components/Home.js
+++ reactApp/components/Home.js
@@ -2,14 +2,29 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Button, Row, Input, Icon } from 'react-materialize';
 
-const Home = props => {
-  return (
-    <div className="home-page">
-      <div>
-        <h2>Welcome home</h2>
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: ''
+    };
+  }
+
+  render() {
+    return (
+      <div className="home-page">
+        <div>
+          <h2>Welcome home</h2>
+          <Row>
+            <Input name="username" type="text" label="username" />
+            <Input name="password" type="password" label="password" />
+            <Button>Login</Button>
+          </Row>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Home;
