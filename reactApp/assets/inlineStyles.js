b89ac4024c68892d7393a9947f8efb60bc8d08e9
Alex <alglaze132@gmail.com>
2018-08-06 15:03:50 -0700

    inline_styles file in assets dir

---
+++ reactApp/assets/inlineStyles.js
@@ -0,0 +1,8 @@
+const INLINE_STYLES = [
+  { style: 'BOLD', icon: 'format_bold' },
+  { style: 'ITALIC', icon: 'format_italic' },
+  { style: 'UNDERLINE', icon: 'format_underline' },
+  { style: 'CODE', icon: 'code' }
+];
+
+export default INLINE_STYLES;
