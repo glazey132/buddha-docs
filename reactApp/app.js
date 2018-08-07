c6abe07a686157af49ae397aa51fa37300566627
Alex <alglaze132@gmail.com>
2018-08-06 18:38:13 -0700

    muithemeprovider wrapper for app

--- reactApp/app.js
+++ reactApp/app.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 import Router from './components/Router';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 require('../css/Draft.css');
 
 ReactDOM.render(
   <BrowserRouter>
-    <Route path={'/'} component={Router} />
+    <MuiThemeProvider>
+      <Route path={'/'} component={Router} />
+    </MuiThemeProvider>
   </BrowserRouter>,
   document.getElementById('root')
 );
